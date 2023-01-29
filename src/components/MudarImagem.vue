9b07e9efda447cb135d6ca1260f17d172f8893c7
Douglas Menchon <114234741+Dougmgm@users.noreply.github.com>
2023-01-28 21:17:03 -0300

    Estudos 28/01

---
+++ src/components/MudarImagem.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <button @click="$emit(mudarImagem)">Trocar imagem de perfil</button> <!--Usar o emit para puxar o metodo mudar imagem-->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MudarImagem",
+        emits: ['mudarImagem']
+    }
+</script>
+
--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,18 +1,31 @@
 <template>
+    <div>
     <img :src="avatar" :alt="descricao"> <!--O "v-bind" está presente mas de forma encurtada, apenas utilizando os dois pontos-->
     <Reutilizacao /> <!--Reutilizado juntamente com o PrimeiroComponente.vue-->
+    <MudarImagem @mudar-imagem="trocarImagem" /> <!--Chamando metodo do emit-->
+
+    </div>
 </template>
 
 <script>
     import Reutilizacao from './Reutilizacao.vue';
+    import MudarImagem from './MudarImagem.vue';
+
     export default {
         name: 'Picture',
         components: {
-            Reutilizacao
+            Reutilizacao,
+            MudarImagem
+        },
+        methods: {
+            trocarImagem() {
+                console.log("teste")
+            }
         },
         data() {
             return {
                 avatar: "/img/avatar.png",
+                avatar2: "/img/avatar2.png",
                 descricao: "Douglas Menchon"
             }
         }
